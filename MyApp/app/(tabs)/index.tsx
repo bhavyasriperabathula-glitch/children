67deae200e4a4212a611411a5faf8b649e54614d
bhavya <bhavya@gmail.com>
2026-04-07 15:26:32 +0530

    first commit

---
+++ MyApp/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { WebView } from "react-native-webview";
+import { SafeAreaView, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: "https://children--bhavyasri2004.replit.app" }}
+        startInLoadingState={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
